cedb59463056783451baeb5b44e0c71cdcb09622
Debayan <73572191+Debayan-creator@users.noreply.github.com>
2024-03-11 17:48:47 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll('.slides img');
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 7000); // Change image every 5 seconds
+}
